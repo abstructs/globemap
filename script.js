4c21a252bf747dc6ac1e070e63ab72b2018efd8f
polygoning <polygoning@gmail.com>
2016-07-15 14:10:10 -0400

    Add tool tip, append circles insteand of paths, create size scales for hovering/mass, on hover now changes colors

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var createMap = function() {
-
-}
-
 var main = function() {
   var margin = {top: 30, bottom: 30, left: 30, right: 30},
   height = 800 - margin.top - margin.bottom,
@@ -17,28 +13,84 @@ var main = function() {
     .append('g')
 
   var projection = d3.geoMercator()
-      .scale(height / 3.14)
+      .scale(height / Math.PI)
       .translate([width / 2, height / 2]);
 
   var path = d3.geoPath()
       .projection(projection);
 
-d3.json('https://raw.githubusercontent.com/mbostock/topojson/master/examples/world-50m.json', function(json) {
+  var div = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+d3.json('https://raw.githubusercontent.com/mbostock/topojson/master/examples/world-50m.json', function(err, json) {
+  if (err) throw err;
   svg.append('g').selectAll('path')
     .data(topojson.feature(json, json.objects.countries).features)
     .enter()
     .append('path')
     .attr('fill', 'black')
+    .style('stroke-width', .2)
+    .attr('stroke', 'red')
     .attr('d', path)
-    d3.json('./meteorite-strike-data.json', function(error, data) {
+    d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json', function(error, data) {
       if (error) throw error;
-      svg.append('g').selectAll('path')
+      var dataScale = d3.scaleLinear()
+          .domain([0, 23000000])
+          .range([1, 50]);
+      var rScale = d3.scaleLinear()
+          .domain([1, 50])
+          .range([5, 55]);
+
+      svg.append('g').selectAll('circle')
       .data(data.features)
       .enter()
-      .append('path')
+      .append('circle')
+      .attr('r', function(d){
+        if (d.properties.mass !== null) {
+          return dataScale(d.properties.mass)
+        }
+      })
+      .attr('cx', function(d){
+        if (d.geometry !== null) {
+          return projection(d.geometry.coordinates)[0]; // x
+        }
+      })
+      .attr('cy', function(d){
+        if (d.geometry !== null) {
+          return projection(d.geometry.coordinates)[1]; // y
+        }
+      })
       .attr('fill', 'red')
-      .style('stroke-width', -1)
+      .style('stroke-width', .2)
+      .attr('stroke', 'black')
       .attr('d', path)
+      .on('mouseover', function(d){
+        d3.select(this).attr('r', function(d){ return rScale(this.getAttribute('r')) })
+        d3.select(this).attr('fill', '#3399ff')
+        d3.select(this).style('stroke-width', 1)
+
+        div.transition()
+            .duration(200)
+            .style("opacity", .9);
+            div.html('Name: ' + d.properties.name + '</br>' +
+                     'Mass: ' + d.properties.mass + '</br>' +
+                     'Longitude: ' + d.geometry.coordinates[0] + '</br>' +
+                     'Latitude: ' + d.geometry.coordinates[1]
+            )
+            .style("left", (d3.event.pageX + 5) + "px")
+            .style("top", (d3.event.pageY - 20) + "px");
+
+      })
+      .on('mouseout', function(){
+        d3.select(this).attr('r', function(d){ return dataScale(d.properties.mass) })
+        d3.select(this).attr('fill', 'red')
+        d3.select(this).style('stroke-width', .2)
+
+        div.transition()
+            .duration(500)
+            .style("opacity", 0);
+      });
     });
   });
 }
